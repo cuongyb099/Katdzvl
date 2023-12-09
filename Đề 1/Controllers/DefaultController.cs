cba33feec45e1e24614d53f14329672407c59c5c
longh <longh@KATDZVL>
2023-12-09 23:16:04 +0700

    Add project files.

---
+++ Đề 1/Controllers/DefaultController.cs
@@ -0,0 +1,68 @@
+﻿using Đề_1.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
+using static System.Web.Razor.Parser.SyntaxConstants;
+
+namespace Đề_1.Controllers
+{
+    public class DefaultController : Controller
+    {
+		Context db = new Context();
+
+		public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RenderNav()
+        {
+            List<NavItems> navItems = db.NavItems.ToList();
+            return PartialView("DaoQuangCuong_Header", navItems);
+        }
+
+        public ActionResult RenderProduct() 
+        {
+            List<Products> products = db.Products.OrderBy(x => x.UnitPrice).Take(4).ToList();
+            return PartialView("DaoQuangCuong_MainContent", products);
+        }
+
+        public ActionResult RenderProductByName(string catName)
+        {
+            List<Products> products = new List<Products>();
+            catName = catName.ToLower();
+            foreach (var item in db.Categories)
+            {
+                if(item.NameVN.ToLower() == catName)
+                {
+                    products = db.Products.Where(h => h.CategoryId == item.Id).ToList();
+                    break;
+                }
+            }
+			return PartialView("DaoQuangCuong_MainContent", products);
+		}
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+		[HttpPost]
+        public ActionResult SignUp(Customers item)
+		{
+			if (ModelState.IsValid)
+			{
+                bool text = item.Password == item.RePassword;
+				db.Customers.Add(item);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View(item);
+			}
+		}
+	}
+}
---
+++ Đề 1/Models/Entities/Customers.cs
@@ -0,0 +1,39 @@
+namespace Đề_1.Models.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Customers
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Customers()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public int Id { get; set; }
+
+		[Required(ErrorMessage = "Không được để trống")]
+		[StringLength(50)]
+        public string Fullname { get; set; }
+
+		[Required(ErrorMessage = "Không được để trống")]
+		[StringLength(50)]
+        public string Email { get; set; }
+
+		[Required(ErrorMessage = "Không được để trống")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+		[Compare("Password")]
+		[Required(ErrorMessage = "Không được để trống")]
+		[DataType(DataType.Password)]
+		public string RePassword { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
